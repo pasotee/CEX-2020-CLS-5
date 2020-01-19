18dc294eadbcdf22451fa77e17a4b918b65ae140
pasotee <oalexprea@yahoo.com>
2020-01-19 14:31:05 +0200

    exercitiile de concurs

--- Vacanta-iarna-2019/Calendar.cpp
+++ Vacanta-iarna-2019/Calendar.cpp
@@ -4,8 +4,54 @@
 
 using namespace std;
 
-int main()
+int main ()
 {
+    int z, l, a;
+    cin >> z >> l >> a;
     
+    // Suntem siguri ca luna trebuie in intervalul 1-12 orice ar fi
+    if(l < 0 || l > 12)
+    {
+        cout << "NU";
+        return 0;
+    }
+
+    int nrZileAleLunii = 0;
+
+    // Tratam luna febraruie separat.
+    if(l==2)
+    {
+        // Feburarie are de obicei 28 de zile, mai putin in anii bisecti.
+        if( (a%4==0 && a%100!=0) || a%400==0)
+        {
+            nrZileAleLunii = 29;
+        }
+        else
+        {
+            nrZileAleLunii = 28;
+        }
+    }
+    else
+    {
+        if(l==1|| l==3 ||l==5 ||l==7 || l==8 || l==10 || l==12)
+        {
+            nrZileAleLunii = 31;
+        }
+        else
+        {
+            nrZileAleLunii = 30;
+        }
+    }
+
+    // Daca si ziua si luna sunt valida, atunci toata data este.
+    if(z>=1 && z<=nrZileAleLunii)
+    {
+        cout<<"DA";
+    }
+    else
+    {
+        cout<<"NU";
+    }
+
     return 0;
 }
--- Vacanta-iarna-2019/Paginare.cpp
+++ Vacanta-iarna-2019/Paginare.cpp
@@ -1,4 +1,4 @@
-//Paginare - https://www.pbinfo.ro/probleme/1002/paginare
+//Salut - https://www.pbinfo.ro/probleme/2201/salut
 
 #include <iostream>
 
@@ -6,6 +6,49 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cin >> n;
+
+    int paginiPerVolum = 300;
+    int cifre0la9 = 1 * 9;          // pt numerele 1-9      folosim cate 1 cifra pt 9 numere    => 1 * 9
+    int cifre10la99 = 2 * 90;       // pt numerele 10-99    folosim cate 2 cifre pt 90 numere   => 2 * 90
+    int cifre100la300 = 3 * 201;    // pt numerele 100-300  folosim cate 3 cifre pt 201 numere  => 3 * 201
+
+    int cifrePerVolum = cifre0la9 + cifre10la99 + cifre100la300;
+
+    int volumeComplete = n / cifrePerVolum;
+    int cifreVolumCurent = n % cifrePerVolum;
+
+    if (cifreVolumCurent == 0)
+    {
+        cout << volumeComplete << " " << paginiPerVolum;
+    }
+    else if (cifreVolumCurent <= cifre0la9)
+    {
+        cout << volumeComplete + 1 << " " << cifreVolumCurent;
+    }
+    else if (cifreVolumCurent <= cifre0la9 + cifre10la99)
+    {
+        if (cifreVolumCurent % 2 == 1)
+        {
+            cout << volumeComplete + 1 << " " << (cifreVolumCurent + 9) / 2;
+        }
+        else
+        {
+            cout << "IMPOSIBIL";
+        }
+    }
+    else // orice peste 3 cifre. 
+    {
+        if (cifreVolumCurent % 3 == 0)
+        {
+            cout << volumeComplete + 1 << " " << (cifreVolumCurent + 9 + 99) / 3;
+        }
+        else
+        {
+            cout << "IMPOSIBIL";
+        }
+    }
+
     return 0;
 }
